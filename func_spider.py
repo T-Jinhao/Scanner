923be16b2521c029820d772502f64dde4572c1a5
keyboy <1184028304@qq.com>
2019-10-21 23:31:13 +0800

    完善

--- func_spider.py
+++ func_spider.py
@@ -2,10 +2,26 @@
 # -*- coding:utf8 -*-
 #author:Jinhao
 
-import requests
+import requests,sys
 import re
 
+
+
 class Spider():
-    def __init__(self,url):
+    def __init__(self,url,cookies):
         self.url = url
-        print(self.url)
+        print(url)
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+        self.cookies = cookies
+        self.spider()
+
+
+    def spider(self):
+        try:
+            res = requests.post(self.url,headers=self.headers,cookies=self.cookies,timeout=10)
+            websites = []
+            print(res.text)
+
+        except:
+            print("网站访问超时")
+            sys.exit(1)
