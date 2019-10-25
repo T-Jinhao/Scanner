aa3522d1bc33f13d8d6fe834a8aacf7e9c47b8da
keyboy <1184028304@qq.com>
2019-10-25 13:37:46 +0800

    优化输出

--- func_spider.py
+++ func_spider.py
@@ -12,7 +12,7 @@ class Spider():
     def __init__(self,url,cookies):
         self.url = url
         print(">>>>>spider"+"-"*40)
-        print("[ 开始爬取网页链接 ] "+url)
+        print("[ 开始爬取网页链接：{}]".format(url))
         self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
         self.cookies = cookies
         self.img,self.web = self.spider()
@@ -29,12 +29,6 @@ class Spider():
             res = requests.post(self.url,headers=self.headers,cookies=self.cookies,timeout=10)
             img_sites = []      # 图片链接
             web_sites = []      # 网站链接
-            # img_results = re.finditer('<img.*?src="(.*?)".*?>',res.text,re.S)
-            # web_results = re.finditer('<a href="(.*?)".*?>',res.text,re.S)
-            # for i in img_results:
-            #     img_sites.append(i.group(1))
-            # for j in web_results:
-            #     web_sites.append(j.group(1))
             soup = BeautifulSoup(res.text,'html.parser')
             img_links = soup.find_all('img')
             web_links = soup.find_all('a')
@@ -44,6 +38,10 @@ class Spider():
             for j in web_links:
                 y = j.get('href')          # 提取href后的链接
                 web_sites.append(y)
+            if not img_sites:
+                img_sites = ''
+            if not web_sites:
+                web_sites = ''
             return img_sites,web_sites
 
         except:
@@ -66,19 +64,19 @@ class Spider():
             os.mkdir(dirpath)
         imgfile = open(imgfilepath,"a")
         try:
-            print("[ 网站图片链接已保存于：{}]".format(imgfilepath))
             for m in self.img:
                 print(m)
                 imgfile.write(m+"\n")
+            print("[ 网站图片链接已保存于：{}]".format(imgfilepath))
         except:
             print("[ 并没有扫描到图片链接 ]")
         imgfile.close()
         webfile = open(webfilepath, "a")
         try:
-            print("[ 网站网页链接已保存于：{}]".format(webfilepath))
             for m in self.web:
                 print(m)
                 webfile.write(m + "\n")
+            print("[ 网站网页链接已保存于：{}]".format(webfilepath))
         except:
             print("[ 并没有扫描到网页链接 ]")
         webfile.close()
