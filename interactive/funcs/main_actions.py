dcd7ae4f78002c915b368736b713941af7b879a1
keyboy <1184028304@qq.com>
2021-04-11 13:56:05 +0800

    完善help功能

--- interactive/funcs/main_actions.py
+++ interactive/funcs/main_actions.py
@@ -3,6 +3,7 @@
 #author:Jinhao
 
 import readline
+import sys
 
 Commands = {
     'usemodule': ['burp', 'scan', 'domain', 'port'],
@@ -19,14 +20,37 @@ Usage = {
 
 def checkIn(enter):
     words = enter.split(' ')
-    if words[0] == 'help':
+    if words[0] == 'usemodule':
+        print('usemodule')
+    elif words[0] == 'works':
+        print('works')
+    elif words[0] == 'exit':
+        sys.exit(0)
+    elif words[0] == 'main':
+        print('main')
+    elif words[0] == 'help':
         printHelp(words)
 
 
 def output(key, value):
     print("{0:10}   {1:10}".format(key, value))
 
+# def completer(text, state, CMD=[]):
+#     options = [cmd for cmd in CMD if cmd.startswith(text)]
+#     if state < len(options):
+#         return options[state]
+#     else:
+#         return None
+#
+# readline.parse_and_bind("tab: complete")
+# readline.set_completer(completer)
+
+def printBanner(banner):
+    print("{0}\n{1}".format(banner, '='*len(banner)))
+    return
+
 def printHelp(words):
+    printBanner('Commands')
     if len(words) == 1 or len(words) > 2:
         for k,v in Usage.items():
             output(k, v)
