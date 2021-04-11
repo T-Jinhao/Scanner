a3b2337f0e2f6930fe6c8260e85e571193277b85
keyboy <1184028304@qq.com>
2021-04-11 18:44:50 +0800

    新增redis模块

---
+++ interactive/funcs/redisUtil.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding:utf8 -*-
+#author:Jinhao
+
+import redis
+from lib.load_config import Config
+
+class Redis:
+    def __init__(self, taskname):
+        config = Config().readConfig()
+        host = config.get("Redis", "host")
+        port = config.get("Redis", "port")
+        password = config.get("Redis", "password")
+        pool = redis.ConnectionPool(host=host, port=port, decode_responses=True, password=password)
+        self.r = redis.Redis(connection_pool=pool)
+        self.taskname = taskname
+
+    def query(self, key):
+        k = self.taskname + '_' + key
+        try:
+            res = self.r.get(k)
+            return res
+        except:
+            return None
+
+    def save(self, key, value):
+        k = self.taskname + '_' + key
+        try:
+            self.r.set(k, value)
+        except:
+            pass
