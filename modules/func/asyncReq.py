62a4101dd39e96406049e14cc7717d048e6e1adc
keyboy <1184028304@qq.com>
2021-04-14 16:43:51 +0800

    添加异步请求方法

--- modules/func/asyncReq.py
+++ modules/func/asyncReq.py
@@ -5,7 +5,7 @@
 import aiohttp
 import asyncio
 import yarl
-from lib.color_output import color_output
+import socket
 from lib.load_config import Config
 
 class Async:
@@ -34,6 +34,76 @@ class Async:
             return {}
 
     def parseUrl(self, url):
+        # 添加协议头
         u = yarl.URL(url)
         if u.scheme == '':
             url += 'http://'
+        return url
+
+    async def getMethod(self, url, **kwargs):
+        u = self.parseUrl(url)
+        try:
+            async with aiohttp.ClientSession(**kwargs) as session:
+                async with session.get(u, **kwargs) as res:
+                    r = Parsing()
+                    await r.parse(res)
+                    return r
+        except:
+            pass
+
+    async def postMethod(self, url, **kwargs):
+        u = self.parseUrl(url)
+        data = kwargs.get('data', {})
+        try:
+            async with aiohttp.ClientSession(**kwargs) as session:
+                async with session.post(u, data=data, **kwargs) as res:
+                    r = Parsing()
+                    await r.parse(res)
+                    return r
+        except:
+            pass
+
+    async def autoRun(self, URLs, Method='GET', **kwargs):
+        '''
+        主要调用方法
+        异步请求
+        :param URLs:
+        :param Method:
+        :param kwargs:
+        :return:
+        '''
+        if type(URLs) != 'list':
+            URLs = [URLs]
+        timeout = kwargs.get('timeout', 3)
+        kwargs['timeout'] = aiohttp.ClientTimeout(total=timeout)
+        if Method == 'POST':
+            tasks = [asyncio.create_task(self.postMethod(u)) for u in URLs]
+        else:
+            tasks = [asyncio.create_task(self.getMethod(u)) for u in URLs]
+        results = await asyncio.gather(*tasks)
+        return results
+
+class Parsing:
+    def __init__(self):
+        self.text = ''
+        self.status = ''
+        self.url = ''
+        self.history = ''
+        self.ip = ''
+
+    async def parse(self, res):
+        self.text = await res.text()
+        self.status = res.status
+        self.url = res.url
+        self.history = res.history
+        self.ip = await self.getIp(self.url)
+
+    async def getIp(self, url):
+        host = yarl.URL(url).host
+        try:
+            ip = socket.gethostbyname(host)
+        except:
+            ip = ''
+        return ip
+
+
