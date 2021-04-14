187a575185dd67571e83135a81c994aaa5d28f43
keyboy <1184028304@qq.com>
2021-04-14 22:44:34 +0800

    终端输出模块

---
+++ modules/terminal/baseModel.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- encoding:utf8 -*-
+#author:Jinhao
+
+class BaseModel:
+    def __init__(self):
+        pass
+
---
+++ modules/terminal/burpTerminal.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- encoding:utf8 -*-
+#author:Jinhao
+
+from modules.terminal import baseModel
+
+class Terminal(baseModel):
+    def __init__(self):
+        pass
