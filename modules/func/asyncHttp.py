08750f6ba83a57a344e41143f8faeef85eb82b10
keyboy <1184028304@qq.com>
2021-04-14 22:34:33 +0800

    数据结构优化

--- modules/func/asyncHttp.py
+++ modules/func/asyncHttp.py
@@ -10,28 +10,38 @@ import socket
 from asyncio import Queue, TimeoutError, gather
 from modules.func import parsing
 
+class req:
+    def __init__(self, timeout=3):
+        self.timeout = timeout
 
-async def fetch(session, url):
-    async with session.get(url) as response:  # session.get(url)获取页面
-        r = parsing.Parsing()
-        await r.parse(response)
-        return r  # 返回页面解码后的信息
+    async def fetch(self, session, url):
+        try:
+            async with session.get(url) as response:  # session.get(url)获取页面
+                r = parsing.Parsing()
+                await r.parse(response)
+                return r  # 返回页面解码后的信息
+        except:
+            pass
 
 
-async def main(url, semaphore):
-    async with semaphore:  # 这里进行执行asyncio.Semaphore，
-        async with aiohttp.ClientSession() as session:
-            html = await fetch(session, url)  # 相当于 yield from
-            print(html.status, html.url)
+    async def main(self, url, semaphore):
+        async with semaphore:  # 这里进行执行asyncio.Semaphore，
+            try:
+                async with aiohttp.ClientSession() as session:
+                    resp = await self.fetch(session, url)  # 相当于 yield from
+                    return resp
+            except:
+                pass
 
-async def run(URLs, workers=500):
-    if type(URLs) != list:
-        URLs = [URLs]
-    semaphore = asyncio.Semaphore(workers)  # 限制并发量为500,这里windows需要进行并发限制，
-    to_get = [main(u, semaphore) for u in URLs]
-    await asyncio.wait(to_get)
+    async def run(self, URLs, workers=500):
+        if type(URLs) != list:
+            URLs = [URLs]
+        semaphore = asyncio.Semaphore(workers)  # 限制并发量为500,这里windows需要进行并发限制，
+        to_get = [self.main(u, semaphore) for u in URLs]
+        await asyncio.wait(to_get)
 
 if __name__ == '__main__':
+    x = req()
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(''))
+    loop.run_until_complete(x.run(''))
     loop.close()
