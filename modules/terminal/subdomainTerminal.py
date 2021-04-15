9fb811e602366e5253e3287f2a482e577498a13a
keyboy <1184028304@qq.com>
2021-04-15 20:10:22 +0800

    增加解析

---
+++ modules/terminal/subdomainTerminal.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- encoding:utf8 -*-
+#author:Jinhao
+
+import sys
+from modules.terminal.baseModel import BaseModel
+from modules.func import util
+
+class Terminal(BaseModel):
+    async def filter(self, resp, url=''):
+        if resp == None:
+            return
+        if self.scanmode:   # 泛解析处理
+            print('pan')
+        else:
+            try:
+                title = util.getTitle(resp.text).replace(' | ', ' || ')
+                msg = "{status} | {protourl} | {title} | {ip} | {url}".format(
+                    status=resp.status,
+                    protourl=resp.protourl,
+                    title=title,
+                    ip=resp.ip,
+                    url=resp.url
+                )
+                output = "".join([
+                    self.Output.green('[ result ] '), self.Output.fuchsia('status_code:'),
+                    self.Output.green(resp.status), self.Output.interval(),
+                    self.Output.fuchsia('初始URL:'), self.Output.green(resp.protourl), self.Output.interval(),
+                    self.Output.fuchsia('标题:'), self.Output.green(title), self.Output.interval(),
+                    self.Output.fuchsia('最终URL:'), str(resp.url),
+                ])
+                print(output)
+                # sys.stdout.flush()
+                return msg
+            except:
+                pass
