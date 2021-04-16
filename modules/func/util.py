5864d079fe939007f61041c889bded7725fe0266
keyboy <1184028304@qq.com>
2021-04-16 14:16:57 +0800

    添加方法

--- modules/func/util.py
+++ modules/func/util.py
@@ -4,7 +4,7 @@
 
 import re
 import socket
-
+from urllib.parse import urljoin
 
 def getHostname(host):
     '''
@@ -30,3 +30,19 @@ def getTitle(text):
     if len(title) > 0:
         return title[0]
     return '[ X ]'
+
+def splicingUrl(url, u):
+    '''
+    检测url完整性，拼接返回绝对地址
+    :param url: 当前扫描的页面url
+    :param u: 获取到的url
+    :return:
+    '''
+    err = ['', None, '/', '\n']
+    if u in err:
+        return
+    if re.match("(http|https)://.*", u):  # 匹配绝对地址
+        return u
+    else:     # 拼凑相对地址，转换成绝对地址
+        u = urljoin(url, u)
+        return u
