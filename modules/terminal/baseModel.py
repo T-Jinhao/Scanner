0e00a53e239335d1674d06023d404ab7327b8426
keyboy <1184028304@qq.com>
2021-04-16 16:35:28 +0800

    方法移动

--- modules/terminal/baseModel.py
+++ modules/terminal/baseModel.py
@@ -25,3 +25,49 @@ class BaseModel:
         if resp == None:
             return
         return resp
+
+    def match_Email(self, text, url):
+        '''
+        匹配邮箱
+        :param text:
+        :return:
+        '''
+        compile_Email = re.compile(r'[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9_-]{0,4}')
+        ret = compile_Email.findall(text)
+        if ret != []:
+            for x in ret:
+                if x not in self.capture_Email and x.split('.')[-1] != 'png':
+                    e = " | ".join([x, url])
+                    self.capture_Email.append(e)
+        return
+
+    def match_ICP(self, text, url):
+        '''
+        匹配ICP备案号
+        :param text:
+        :return:
+        '''
+        compile_ICP = re.compile("([\u4e00-\u9fa5]ICP备\d{8}号-([0-9]|10))")
+        ret = compile_ICP.search(text)
+        try:
+            if ret != None and ret[0] not in self.capture_ICP and ret[0] != []:
+                i = " | ".join([ret[0], url])
+                self.capture_ICP.append(i)
+        except:
+            pass
+        return
+
+    def match_Phone(self, text, url):
+        '''
+        匹配手机号码
+        :param text:
+        :return:
+        '''
+        compile_Phone = re.compile(r'1[3456789]\d{9}')
+        ret = compile_Phone.findall(text)
+        if ret != []:
+            for x in ret:
+                if x not in self.capture_Phone:
+                    p = " | ".join([str(x), url])
+                    self.capture_Phone.append(p)
+        return
