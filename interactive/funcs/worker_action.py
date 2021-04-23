93c10e15f473444ad5301bd7c901881d5f80cbe7
keyboy <1184028304@qq.com>
2021-04-23 14:00:57 +0800

    修改

--- interactive/funcs/worker_action.py
+++ interactive/funcs/worker_action.py
@@ -2,3 +2,29 @@
 # -*- coding:utf8 -*-
 #author:Jinhao
 
+import threading
+from interactive.funcs import util
+
+Commands = {
+    'info': [],
+    'set': [],
+    'usemodule': ['burp', 'scan', 'domain', 'port'],
+    'run': [],
+    'main': [],
+    'help': [],
+    'exit': []
+}
+
+Usage = {
+    'info': 'Display worker module options.',
+    'help': 'Displays the help menu.',
+    'exit': 'Exit Scanner.',
+    'main': 'Back to the main menu.',
+    'run': 'Start the given worker module.',
+    'usemodule': 'Use a Scanner module.',
+    'set': 'Set a worker option.'
+}
+
+Info = {
+    'Taskname': ['False', '', 'The uniquely identifies of current work.']
+}
