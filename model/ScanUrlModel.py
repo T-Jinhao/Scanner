e4630ebdd3a5bbcec644130db13ac8fddcf62c8c
keyboy <1184028304@qq.com>
2021-05-02 18:00:24 +0800

    对接数据库保存接口

--- model/ScanUrlModel.py
+++ model/ScanUrlModel.py
@@ -8,10 +8,10 @@ class ScanUrlModel(BaseModel):
     taskname = CharField()
     # taskname唯一编号
     tasknameid = CharField()
-    # 捕获的url
-    current_url = CharField()
-    # 捕获的url
-    capture_url = CharField()
+    # # 捕获的url
+    # current_url = CharField()
+    # 扫描的url
+    url = CharField()
     # 状态码
     status_code = IntegerField()
     # 文本长度
