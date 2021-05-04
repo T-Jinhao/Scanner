c07bcc0f11fe37b35eef5b051bf9365b716982f1
keyboy <1184028304@qq.com>
2021-05-04 15:57:31 +0800

    添加任务数据库

---
+++ model/TaskModel.py
@@ -0,0 +1,14 @@
+from peewee import *
+from model.BaseModel import BaseModel
+
+class TaskModel(BaseModel):
+    # 时间
+    timestamp = DateTimeField()
+    # 任务名
+    taskname = CharField()
+    # taskname唯一编号
+    tasknameid = CharField()
+    # 描述
+    description = CharField()
+    # 目标
+    target = CharField()
